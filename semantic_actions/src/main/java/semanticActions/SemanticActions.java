12fd8a8653b4ccfadbd0e1c9962ac1e751cdedc5
jayce-leathers <me@jayceleathers.com>
2016-04-22 09:18:27 -0400

    wip implementation currently debugging

---
+++ semantic_actions/src/main/java/semanticActions/SemanticActions.java
@@ -0,0 +1,177 @@
+package semanticActions;
+
+import static errors.SemanticError.*;
+import errors.SemanticError;
+
+import errors.SymbolTableError;
+import lex.Token;
+import lex.TokenType;
+import lex.Tokenizer;
+import symbolTable.*;
+
+import java.util.Stack;
+
+public class SemanticActions {
+
+	private Stack<Object> semanticStack ;
+	private boolean insert ;
+	private boolean isArray ;
+	private boolean global ;
+	private int globalMemory ;
+	private int localMemory ;
+
+	private SymbolTable globalTable ;
+	private SymbolTable localTable ;
+
+	private SymbolTable constantTable ;
+
+	private int tableSize = 97;
+	private boolean isParam;
+	private SymbolTableEntry nullEntry = null;
+	private Tokenizer tokenizer;
+
+	public SemanticActions(Tokenizer tokenizer) {
+		semanticStack = new Stack<Object>();
+		insert = false;
+		isArray = false;
+		isParam = false;
+		global = true;
+		globalMemory = 0 ;
+		localMemory = 0;
+		globalTable = new SymbolTable(tableSize);
+		constantTable = new SymbolTable(tableSize);
+		installBuiltins(globalTable);
+		this.tokenizer = tokenizer;
+	}
+
+	public void execute(int actionNumber, Token token) throws SemanticError {
+
+		debug("calling action : " + actionNumber + " with token " + token.getType() + " with value " + token.getValue());
+
+		// TODO Implement actions 1, 2, 3, 4, 6, 7, 9, and 13.
+		switch (actionNumber)
+		{
+
+			case 1:
+				this.insert = true;
+				break;
+			case 2:
+				this.insert = false;
+				break;
+			case 3:
+				TokenType type = ((Token)semanticStack.pop()).getType();
+				if(isArray){
+					int upperBound = Integer.parseInt(((Token)semanticStack.pop()).getValue());
+					int lowerBound = Integer.parseInt(((Token)semanticStack.pop()).getValue());
+					int memSize = (upperBound - lowerBound) + 1;
+					Token id;
+					while(!semanticStack.isEmpty()){
+						id = (Token)semanticStack.pop();
+						int address;
+						if(global) {
+							address = globalMemory;
+							globalMemory = globalMemory + memSize;
+							try {
+								globalTable.insert(new ArrayEntry(id.getValue(),type,address,upperBound,lowerBound));
+							} catch (SymbolTableError symbolTableError) {
+								throw MultipleDeclaration(id.getValue(),tokenizer.getLineNumber());
+							}
+						}
+						else {
+							address = localMemory;
+							localMemory = localMemory + memSize;
+//							localTable.insert(new ArrayEntry(id.getValue(),id.getType(),address,upperBound,lowerBound));
+						}
+					}
+				}
+				else {
+					Token id;
+					while(!semanticStack.isEmpty()){
+						id = (Token)semanticStack.pop();
+						int address;
+						if(global) {
+							address = globalMemory;
+							globalMemory++;
+							try {
+								globalTable.insert(new VariableEntry(id.getValue(),type,address));
+							} catch (SymbolTableError symbolTableError) {
+								throw MultipleDeclaration(id.getValue(),tokenizer.getLineNumber());
+							}
+						}
+						else {
+							address = localMemory;
+							localMemory++;
+//							localTable.insert(new VariableEntry(id.getValue(),id.getType(),address));
+						}
+					}
+				}
+				break;
+			case 4:
+				semanticStack.push(token);
+				break;
+			case 6:
+				this.isArray = true;
+				break;
+			case 7:
+				semanticStack.push(token);
+				break;
+			case 9:
+				Token id1 = (Token) semanticStack.pop();
+				IODeviceEntry entry1 = new IODeviceEntry(id1.getValue());
+				entry1.setReserved(true);
+				try {
+					globalTable.insert(entry1);
+				} catch (SymbolTableError symbolTableError) {
+					throw MultipleDeclaration(id1.getValue(),tokenizer.getLineNumber());
+				}
+				Token id2 = (Token) semanticStack.pop();
+				IODeviceEntry entry2 = new IODeviceEntry(id2.getValue());
+				entry2.setReserved(true);
+				try {
+					globalTable.insert(entry2);
+				} catch (SymbolTableError symbolTableError) {
+					throw MultipleDeclaration(id2.getValue(),tokenizer.getLineNumber());
+				}
+				Token id3 = (Token) semanticStack.pop();
+				ProcedureEntry entry3 = new ProcedureEntry(id3.getValue(),id3.getType(),0,null);
+				entry3.setReserved(true);
+				try {
+					globalTable.insert(entry3);
+				} catch (SymbolTableError symbolTableError) {
+					throw MultipleDeclaration(id2.getValue(),tokenizer.getLineNumber());
+				}
+				insert = false;
+				break;
+			case 13:
+				semanticStack.push(token);
+				break;
+
+
+			default:
+				// TODO Eventually (i.e. final project) this should throw an exception.
+				debug("Action " + actionNumber + " not yet implemented.");
+
+		}
+	}
+
+	public SymbolTableEntry lookup(String name)
+	{	return globalTable.lookup(name);
+	}
+
+	public ConstantEntry lookupConstant(Token token)
+	{
+		return (ConstantEntry)constantTable.lookup(token.getValue());
+	}
+
+	private void installBuiltins(SymbolTable table)
+	{
+		SymbolTable.installBuiltins(table);
+	}
+
+
+	private void debug(String message) {
+		// TODO Uncomment the following line to enable debug output.
+		System.out.println(message);
+	}
+
+}
